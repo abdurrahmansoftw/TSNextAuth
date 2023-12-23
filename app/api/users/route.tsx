069da59c4ec06d873b23de133e1430a2986356ac
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-24 00:07:45 +0600

    post to create objects

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,13 @@ export function GET(request: NextRequest) {
         { id: 2, name: "John Doe" },
     ]);
 }
+
+export async function POST(request: NextRequest) {
+    const body  = await request.json();
+    // validate body
+    // if invalid, return NextResponse.json({error: "Invalid body"}, {status: 400})
+    if (!body.name)
+        return NextResponse.json({ error: "Name is required" }, { status: 400 });
+    
+    return NextResponse.json({id: 3, name: body.name});
+}
