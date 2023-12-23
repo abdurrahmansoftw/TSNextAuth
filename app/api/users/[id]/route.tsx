196a8f80feec5210e86654f497608b681499212e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-23 23:56:58 +0600

    Getting a Single Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,10 +1,7 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function get(req: NextRequest) {
-    return {
-        body: [
-            { id: 1, name: "akaid Doe" },
-            { id: 2, name: "John Doe" },
-        ],
-    };
+export function get(req: NextRequest, params: { id: number }) {
+    if (params.id > 10)
+        return NextResponse.json({ error: "User not found" }, { status: 404 });
+    return NextResponse.json({ id: params.id, name: "John Doe" });
 }
