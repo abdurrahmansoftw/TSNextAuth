5680b60757d3f9d4011e1cd12257a9e9ca125478
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-23 23:57:13 +0600

    User not found

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -3,5 +3,5 @@ import { NextRequest, NextResponse } from "next/server";
 export function get(req: NextRequest, params: { id: number }) {
     if (params.id > 10)
         return NextResponse.json({ error: "User not found" }, { status: 404 });
-    return NextResponse.json({ id: params.id, name: "John Doe" });
+    return NextResponse.json({ id: params.id, name: "aakid Doe" });
 }
