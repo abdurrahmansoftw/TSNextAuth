36dcd3a192ff4a9224496e7c492db6ad196bd0b1
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-24 15:35:57 +0600

    Sending Emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,16 +1,22 @@
-import { Body, Container, Html, Preview, Text } from "@react-email/components";
+import { Body, Container, Html, Preview, Tailwind, Text } from "@react-email/components";
 import Link from "next/link";
 
-const WelcomeTemplate = ({name} : {name:string}) => {
+const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
-        <Preview> Welcome  Abourd</Preview>
-        <Body>
-        <Container> 
-            <Text> Hello {name}</Text>
-              <Link href="https://abdurrahmans.netlify.app" > www.abdurrahmans.neltify.app</Link>
-        </Container>
+      <Preview>Welcome aboard!</Preview>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">
+              Hello {name}
+            </Text>
+            <Link href="https://codewithmosh.com">
+              www.codewithmosh.com
+            </Link>
+          </Container>
         </Body>
+      </Tailwind>
     </Html>
   );
 };
