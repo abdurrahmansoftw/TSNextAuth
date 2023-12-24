d4e3411ac2d737f5d174f27cb1a92220e7f604d5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-24 10:52:03 +0600

    products Add

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,18 +1,21 @@
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import scheme from './schema';
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-      { id: 1, name: "milk", price: 2.99 },
-      { id: 2, name: "bread", price: 3.99 },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany()
+  return NextResponse.json(products)
 }
 
 export async function POST(request: NextRequest) {
+
   const body  = await request.json();
   const validation =  scheme.safeParse(body);
+  
   if (!validation.success)
       return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json({id: 3, name: body.name, price: body.price}, { status: 201 })
+ const newProduct = await prisma.product.create({data: {name: body.name, price: body.price}})
+
+  return NextResponse.json(newProduct, { status: 201 })
 }
