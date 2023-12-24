f1d7c740489eb92cb40935d32a7fa490ecec4662
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-24 16:03:39 +0600

    Using Third-party Scripts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default async function Home() {
-  return <div>
-    <Image src='https://bit.ly/react-cover'  fill alt='coffer' className="object-cover" sizes="(max-width: 480px), 100vw, (max-width: 768px) 50vw, 33w " />
-  </div>;
+  return <main className="relative h-screen">
+    <Image src='https://bit.ly/react-cover'  fill alt='coffer' className="object-cover" sizes="(max-width: 480px), 100vw, (max-width: 768px) 50vw, 33w " quality={75} priority />
+  </main>;
 }
