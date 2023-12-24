036de1668a781eee64965aa1c8175011782af99f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-24 17:36:01 +0600

    add mysql database again

--- prisma/migrations/20231224113443_add_agaian_mysql/migration.sql
+++ prisma/migrations/20231224113443_add_agaian_mysql/migration.sql
@@ -34,6 +34,7 @@ CREATE TABLE `User` (
     `name` VARCHAR(191) NULL,
     `email` VARCHAR(191) NULL,
     `emailVerified` DATETIME(3) NULL,
+    `hashedPassword` VARCHAR(191) NULL,
     `image` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
