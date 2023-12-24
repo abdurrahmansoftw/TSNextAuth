59715e85cccc0b872ecdacc8598ecdd6bbf822ce
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-24 12:43:28 +0600

    Configuring Google Provider

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <div className='flex p-5 space-x-5'>
+        <div className='flex p-5 space-x-3'>
             <Link href= '/' className="mr-5"> next.ts </Link>
             <Link href= '/users' className="mr-5"> Users </Link>
             <Link href= '/api/auth/signin' className="mr-5"> Login </Link>
