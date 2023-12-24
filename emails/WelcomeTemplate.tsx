086e85ab8ef3cfa52756d0e766849969cc05710d
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-24 15:13:14 +0600

    Previewing Emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 const WelcomeTemplate = ({name} : {name:string}) => {
   return (
     <Html>
-    <Preview> Welcome  Abourd</Preview>
-    <Body>
-      <Container> 
-        <Text> Hello {name}</Text>
-        <Link href="https://abdurrahmans.netlify.app" > www.abdurrahmans.neltify.app</Link>
+        <Preview> Welcome  Abourd</Preview>
+        <Body>
+        <Container> 
+            <Text> Hello {name}</Text>
+              <Link href="https://abdurrahmans.netlify.app" > www.abdurrahmans.neltify.app</Link>
         </Container>
-    </Body>
-</Html>
+        </Body>
+    </Html>
   );
 };
 
